52591acb9fd754ec02674bd47daae1896b676082
Rhapss <45916288+Rhapss@users.noreply.github.com>
2020-10-07 02:05:20 +0300

    Lab 1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace Lab1
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            int r1 = 0, r2 = 0, r3 = 0;
+            Console.WriteLine("Введите сопротивление 3х резисторов:");
+            Console.Write("R1:");
+            r1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("R2:");
+            r2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("R3:");
+            r3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Сопротивление трёх последовательно соединённых резисторов будет составлять: {r1+r2+r3}Ом");
         }
     }
 }
