b79183c68748d8f5f7ebf15d5f10766356f2a25d
aldecode <reshetnik.nikita@gmail.com>
2021-06-09 17:43:42 +0300

    Lab 13

---
+++ Lab13/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Lab13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TabControl = new System.Windows.Forms.TabControl();
+            this.TabPage1 = new System.Windows.Forms.TabPage();
+            this.ComboBox = new System.Windows.Forms.ComboBox();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.TabPage2 = new System.Windows.Forms.TabPage();
+            this.PanelNumbers = new System.Windows.Forms.Panel();
+            this.TabControl.SuspendLayout();
+            this.TabPage1.SuspendLayout();
+            this.TabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TabControl
+            // 
+            this.TabControl.Controls.Add(this.TabPage1);
+            this.TabControl.Controls.Add(this.TabPage2);
+            this.TabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TabControl.Location = new System.Drawing.Point(0, 0);
+            this.TabControl.Name = "TabControl";
+            this.TabControl.SelectedIndex = 0;
+            this.TabControl.Size = new System.Drawing.Size(208, 242);
+            this.TabControl.TabIndex = 0;
+            // 
+            // TabPage1
+            // 
+            this.TabPage1.Controls.Add(this.ComboBox);
+            this.TabPage1.Controls.Add(this.RemoveButton);
+            this.TabPage1.Controls.Add(this.AddButton);
+            this.TabPage1.Controls.Add(this.TextBox);
+            this.TabPage1.Controls.Add(this.Label1);
+            this.TabPage1.Location = new System.Drawing.Point(4, 24);
+            this.TabPage1.Name = "TabPage1";
+            this.TabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.TabPage1.Size = new System.Drawing.Size(200, 214);
+            this.TabPage1.TabIndex = 0;
+            this.TabPage1.Text = "Task 1";
+            this.TabPage1.UseVisualStyleBackColor = true;
+            // 
+            // ComboBox
+            // 
+            this.ComboBox.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ComboBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ComboBox.ForeColor = System.Drawing.SystemColors.Info;
+            this.ComboBox.FormattingEnabled = true;
+            this.ComboBox.Location = new System.Drawing.Point(3, 106);
+            this.ComboBox.Name = "ComboBox";
+            this.ComboBox.Size = new System.Drawing.Size(194, 23);
+            this.ComboBox.TabIndex = 4;
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.RemoveButton.Location = new System.Drawing.Point(3, 66);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(194, 40);
+            this.RemoveButton.TabIndex = 3;
+            this.RemoveButton.Text = "Delete";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            this.RemoveButton.Click += new System.EventHandler(this.RemoveButton_Click);
+            // 
+            // AddButton
+            // 
+            this.AddButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AddButton.Location = new System.Drawing.Point(3, 26);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(194, 40);
+            this.AddButton.TabIndex = 2;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // TextBox
+            // 
+            this.TextBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.TextBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TextBox.Location = new System.Drawing.Point(3, 3);
+            this.TextBox.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(194, 23);
+            this.TextBox.TabIndex = 1;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(96, -15);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(96, 15);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "Enter text here:";
+            this.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TabPage2
+            // 
+            this.TabPage2.Controls.Add(this.PanelNumbers);
+            this.TabPage2.Location = new System.Drawing.Point(4, 24);
+            this.TabPage2.Name = "TabPage2";
+            this.TabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.TabPage2.Size = new System.Drawing.Size(200, 214);
+            this.TabPage2.TabIndex = 1;
+            this.TabPage2.Text = "Task 2";
+            this.TabPage2.UseVisualStyleBackColor = true;
+            // 
+            // PanelNumbers
+            // 
+            this.PanelNumbers.AutoScroll = true;
+            this.PanelNumbers.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelNumbers.Location = new System.Drawing.Point(3, 3);
+            this.PanelNumbers.Name = "PanelNumbers";
+            this.PanelNumbers.Size = new System.Drawing.Size(194, 244);
+            this.PanelNumbers.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(208, 242);
+            this.Controls.Add(this.TabControl);
+            this.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.Name = "Form1";
+            this.Text = "Lab 2";
+            this.TabControl.ResumeLayout(false);
+            this.TabPage1.ResumeLayout(false);
+            this.TabPage1.PerformLayout();
+            this.TabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl TabControl;
+        private System.Windows.Forms.TabPage TabPage1;
+        private System.Windows.Forms.TabPage TabPage2;
+        private System.Windows.Forms.ComboBox ComboBox;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Panel PanelNumbers;
+    }
+}
---
+++ Lab13/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Lab13
+{
+    public partial class Form1 : Form
+    {
+        private const int ButtonCounter = 16;
+        private int _numberNow = 1;
+        private List<int> numbers;
+        HashSet<int> tempNumbers;
+        Random random;
+        Stopwatch stopWatch;
+
+        public Form1()
+        {
+            InitializeComponent();
+            numbers = new List<int>(ButtonCounter);
+            tempNumbers = new HashSet<int>(ButtonCounter);
+            random = new Random();
+            stopWatch = new Stopwatch();
+            CreateButtons();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            if(TextBox.Text != string.Empty)
+            {
+                ComboBox.Items.Add(TextBox.Text);
+                TextBox.BackColor = Color.White;
+                TextBox.ForeColor = Color.Black;
+                TextBox.Text = string.Empty;
+            }
+            else
+            {
+                MessageBox.Show("Ведіть текст!");
+                TextBox.BackColor = Color.Red;
+                TextBox.ForeColor = Color.White;
+            }
+        }
+
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text != string.Empty)
+            {
+                if(ComboBox.Items.Contains(TextBox.Text))
+                {
+                    ComboBox.Items.Remove(TextBox.Text);
+                    TextBox.BackColor = Color.White;
+                    TextBox.ForeColor = Color.Black;
+                    TextBox.Text = string.Empty;
+                }
+                else
+                {
+                    MessageBox.Show("Такого тексту немає.");
+                    TextBox.BackColor = Color.Red;
+                    TextBox.ForeColor = Color.White;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ведіть текст!");
+                TextBox.BackColor = Color.Red;
+                TextBox.ForeColor = Color.White;
+            }
+        }
+
+        private void CreateButtons()
+        {
+            numbers.AddRange(Enumerable.Range(1, ButtonCounter));
+
+            int size = 40;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < ButtonCounter / 4; j++)
+                {
+                    Button button = new Button
+                    {
+                        Location = new Point((size + 10) * i, (size + 10) * j),
+                        Size = new Size(size, size)
+                    };
+                    button.Click += new EventHandler(ButtonClick);
+                    int number = RandomNumber(0, ButtonCounter);
+                    tempNumbers.Add(number);
+
+                    button.Text = number.ToString();
+                    PanelNumbers.Controls.Add(button);
+                }
+            }
+
+            tempNumbers.Clear();
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            Button button = sender as Button; 
+            if(_numberNow == int.Parse(button?.Text))
+            {
+                if(_numberNow == 1)
+                {
+                    stopWatch.Start();
+                }
+                if (_numberNow == 16)
+                {
+                    TimeSpan ts = stopWatch.Elapsed;
+                    string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+                    stopWatch.Reset();
+                    ResetButton();
+                    return;
+                }
+                button.Visible = false;
+                _numberNow++;
+                ChangeNumber();
+            }
+            else
+            {
+                ResetButton();
+            }
+        }
+
+        private void ResetButton()
+        {
+            _numberNow = 1;
+            PanelNumbers.Controls.Clear();
+            CreateButtons();
+        }
+
+        private void ChangeNumber()
+        {
+            foreach (var item in PanelNumbers?.Controls)
+            {
+                Button button = item as Button;
+                if (button != null && button.Visible)
+                {
+                    int number = RandomNumber(_numberNow-1, ButtonCounter);
+                    tempNumbers.Add(number);
+                    button.Text = number.ToString();
+                }
+            }
+            tempNumbers.Clear();
+        }
+
+        private int RandomNumber(int min,int max)
+        {
+            int number;
+            do
+            {
+                int index = random.Next(min, max);
+                number = numbers[index];
+            } while (tempNumbers.Contains(number));
+            return number;
+        }
+        
+    }
+}
